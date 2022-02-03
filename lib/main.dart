f6b54897b9c08ca775a69288ddfdd873fe457eb2
akshitkumarsharma <kakshit2543@gmail.com>
2022-02-04 01:01:54 +0530

    Added Floating action button, navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -36,114 +36,179 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: Colors.blue,
       body: Container(
-        padding: EdgeInsets.fromLTRB(18, 40, 0, 0),
+        padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Row(
               children: [
-                Icon(
-                  Icons.calendar_today,
-                  color: Colors.white,
-                ),
                 SizedBox(
-                  width: 5,
+                  width: 20,
                 ),
-                Text(
-                  "2022-01 Balance",
-                  style: TextStyle(color: Colors.white),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.calendar_today,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "2022-01 Balance",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "-689",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 38,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Expense: ",
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        Text(
+                          "-689",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w800),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Income: ",
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        Text(
+                          "0",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                              fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ],
             ),
-            Text(
-              "-689",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 38,
-                fontWeight: FontWeight.w700,
+            SizedBox(
+              height: 20,
+            ),
+            Card(
+              margin: EdgeInsets.zero,
+              elevation: 15,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 14, 8, 10),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Budget:",
+                          style: TextStyle(fontWeight: FontWeight.w900),
+                        ),
+                        Text(
+                          "1,220",
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.w900),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    /*LinearProgressIndicator(
+                      color: Colors.blue,
+                      minHeight: 8,
+                    ),*/
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 180,
+                        ),
+                        Text(
+                          "Remaining Budget:",
+                          style: TextStyle(fontWeight: FontWeight.w900),
+                        ),
+                        Text(
+                          "531",
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.w900),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-            Row(
-              children: [
-                Text(
-                  "Expense: ",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                Text(
-                  "-689",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w800),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "Income: ",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-                Text(
-                  "0",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w800,
-                      fontSize: 16),
-                ),
-              ],
-            ),
             Container(
+              height: 417,
               color: Colors.grey,
+              padding: EdgeInsets.all(10),
               child: Column(
                 children: [
-                  Card(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 14, 8, 10),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                "Budget:",
-                                style: TextStyle(fontWeight: FontWeight.w900),
-                              ),
-                              Text(
-                                "1,220",
-                                style: TextStyle(
-                                    fontSize: 22, fontWeight: FontWeight.w900),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          /*LinearProgressIndicator(
-                            color: Colors.blue,
-                            minHeight: 8,
-                          ),*/
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 150,
-                              ),
-                              Text(
-                                "Remaining Budget:",
-                                style: TextStyle(fontWeight: FontWeight.w900),
-                              ),
-                              Text(
-                                "531",
-                                style: TextStyle(
-                                    fontSize: 22, fontWeight: FontWeight.w900),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
+                  Container(
+                    color: Colors.white,
+                    child: Row(
+                      children: [
+                        Text("Feb 03, 2022"),
+                        SizedBox(
+                          width: 165,
+                        ),
+                        Text("Expense:"),
+                        Text("-689"),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 5, top: 10),
+                    color: Colors.white,
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.watch,
+                          color: Colors.red,
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "Fruits",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, fontSize: 18),
+                        ),
+                        SizedBox(
+                          width: 210,
+                        ),
+                        Text(
+                          "-500",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, fontSize: 18),
+                        ),
+                      ],
                     ),
                   ),
                 ],
@@ -151,6 +216,38 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          size: 35,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            title: Text(""),
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text(""),
+            backgroundColor: Colors.grey,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart),
+            title: Text(""),
+            backgroundColor: Colors.grey,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text(""),
+            backgroundColor: Colors.grey,
+          ),
+        ],
       ),
     );
   }
